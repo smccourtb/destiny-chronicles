c33c5e28b079d2095f1d6d9684d2d4b1ddf20aa3
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-04-05 01:02:45 -0400

    implement headless ui radio group for perk columns

---
+++ components/items/weapons/PerkColumn.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { FormattedPerks } from '../../../lib/items'
+import { RadioGroup } from '@headlessui/react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../../Tooltip'
+import Image from 'next/image'
+import { applyBungieDomain } from '../../../utils/url-handling'
+import PerkTooltip from '../../PerkTooltip'
+
+type PerkColumnProps = {
+  perks: FormattedPerks[]
+}
+const PerkColumn = ({ perks }: PerkColumnProps) => {
+  const [activePerk, setActivePerk] = useState<FormattedPerks | null>(null)
+  const perkOptions = perks.map((perk) => {
+    return (
+      <RadioGroup.Option key={perk.hash} value={perk} className={'rounded-full'}>
+        {({ checked, active }) => (
+          <Tooltip key={perk.hash}>
+            <TooltipTrigger asChild={true}>
+              <Image
+                src={applyBungieDomain(perk.displayProperties.icon)}
+                width={48}
+                height={48}
+                alt={`${perk.displayProperties.name}`}
+                onClick={() => checked && setActivePerk(null)}
+                className={`${checked ? 'bg-modifier-blue' : 'bg-transparent'} ${
+                  active ? 'ring-2 ring-modifier-blue' : ''
+                } rounded-full p-1 border border-[#707070] hover:ring-2 hover:ring-modifier-blue`}
+              />
+            </TooltipTrigger>
+            <TooltipContent>
+              <PerkTooltip
+                name={perk.displayProperties.name}
+                type={perk.itemTypeDisplayName}
+                description={perk.displayProperties.description}
+              />
+            </TooltipContent>
+          </Tooltip>
+        )}
+      </RadioGroup.Option>
+    )
+  })
+  return (
+    <RadioGroup value={activePerk} onChange={setActivePerk} className="flex flex-col gap-2">
+      <RadioGroup.Label className="sr-only">{perks[0].itemTypeDisplayName}</RadioGroup.Label>
+      {perkOptions}
+    </RadioGroup>
+  )
+}
+
+export default PerkColumn
--- components/items/weapons/WeaponPerkGrid.tsx
+++ components/items/weapons/WeaponPerkGrid.tsx
@@ -1,42 +1,13 @@
 import React from 'react'
-import Image from 'next/image'
-import { applyBungieDomain } from '../../../utils/url-handling'
 import { FormattedPerks } from '../../../lib/items'
-import { Tooltip, TooltipContent, TooltipTrigger } from '../../Tooltip'
-import PerkTooltip from '../../PerkTooltip'
+import PerkColumn from './PerkColumn'
 
 type WeaponPerkGridProps = {
   perks: FormattedPerks[][]
 }
 const WeaponPerkGrid = ({ perks }: WeaponPerkGridProps) => {
   const perkGrid = perks.map((perkColumn) => {
-    return (
-      <div key={perkColumn[0].hash} className="flex flex-col gap-4 w-fit">
-        {perkColumn.map((perk) => {
-          return (
-            <Tooltip key={perk.hash}>
-              <TooltipTrigger>
-                <Image
-                  src={applyBungieDomain(perk.displayProperties.icon)}
-                  width={48}
-                  height={48}
-                  alt={`${perk.displayProperties.name}`}
-                  onClick={() => console.log('clicked')}
-                  className="inline-block active:bg-modifier-blue rounded-full border border-[#707070] focus:border-modifier-blue p-1"
-                />
-              </TooltipTrigger>
-              <TooltipContent>
-                <PerkTooltip
-                  name={perk.displayProperties.name}
-                  type={perk.itemTypeDisplayName}
-                  description={perk.displayProperties.description}
-                />
-              </TooltipContent>
-            </Tooltip>
-          )
-        })}
-      </div>
-    )
+    return <PerkColumn key={perkColumn[0].hash} perks={perkColumn} />
   })
   return <section className="grid grid-cols-6 gap-8 w-fit">{perkGrid}</section>
 }
