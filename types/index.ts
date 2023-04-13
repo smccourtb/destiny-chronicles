15e258bbe26531f3a94841be9cce4ceed31d100d
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-04-12 21:15:49 -0400

    add new type for weapon type and subtype

--- types/index.ts
+++ types/index.ts
@@ -466,3 +466,68 @@ export type DestinyItemTooltipNotification = {
   displayString: string
   displayStyle: string
 }
+
+export enum DestinyItemSubType {
+  None = 0,
+  Crucible = 1, // DEPRECATED. Items can be both "Crucible" and something else interesting.
+  Vanguard = 2, // DEPRECATED. An item can both be "Vanguard" and something else.
+  Exotic = 5, // DEPRECATED. An item can both be Exotic and something else.
+  AutoRifle = 6,
+  Shotgun = 7,
+  Machinegun = 8,
+  HandCannon = 9,
+  RocketLauncher = 10,
+  FusionRifle = 11,
+  SniperRifle = 12,
+  PulseRifle = 13,
+  ScoutRifle = 14,
+  Crm = 16, // DEPRECATED. An item can both be CRM and something else.
+  Sidearm = 17,
+  Sword = 18,
+  Mask = 19,
+  Shader = 20,
+  Ornament = 21,
+  FusionRifleLine = 22,
+  GrenadeLauncher = 23,
+  SubmachineGun = 24,
+  TraceRifle = 25,
+  HelmetArmor = 26,
+  GauntletsArmor = 27,
+  ChestArmor = 28,
+  LegArmor = 29,
+  ClassArmor = 30,
+  Bow = 31,
+  DummyRepeatableBounty = 32,
+  Glaive = 33,
+}
+
+export enum DestinyItemType {
+  None = 0,
+  Currency = 1,
+  Armor = 2,
+  Weapon = 3,
+  Message = 7,
+  Engram = 8,
+  Consumable = 9,
+  ExchangeMaterial = 10,
+  MissionReward = 11,
+  QuestStep = 12,
+  QuestStepComplete = 13,
+  Emblem = 14,
+  Quest = 15,
+  Subclass = 16,
+  ClanBanner = 17,
+  Aura = 18,
+  Mod = 19,
+  Dummy = 20,
+  Ship = 21,
+  Vehicle = 22,
+  Emote = 23,
+  Ghost = 24,
+  Package = 25,
+  Bounty = 26,
+  Wrapper = 27,
+  SeasonalArtifact = 28,
+  Finisher = 29,
+  Pattern = 30,
+}
